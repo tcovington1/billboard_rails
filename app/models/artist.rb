76783b804a8fe70b18ae329123ab9fd955c251f5
Taylor Covington <tcovingtondev@gmail.com>
2019-09-04 13:19:46 -0600

    project working, added controllers, navbar, and view pages

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ApplicationRecord
+  has_many :songs
+  has_many :billboards, through: :songs
+  
+  
+  validates :first_name, :last_name, :age, presence: true
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+billboards = ['billboard', 'us', 'international']
+10.times do
+  song = Song.create(
+    title: Faker::Verb.past,
+    top: Faker::Boolean.boolean,
+    billboard_id: billboard.id,
+    artist_id: artist.id
+  )
+
+  4.times do 
+    artist = Artist.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      age: Faker::Number.within(range: 1..100)
+    )
+
+    
+    Billboard.create(
+      billboard_name: billboards.sample,
+      
+    )
+  end
+end
+
+puts 'Data seeded.'
